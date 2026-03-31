e8ad8a2fca5b9e92aaa9458c719bfa17b017906a
Murilo312 <murilosouzaferr@gmail.com>
2026-03-30 22:45:08 -0400

    essas aulas sao testes que quero fazer com os assuntos das aulas

---
+++ aulas/aula02.py
@@ -0,0 +1,4 @@
+salario = float(input('qual seu salario '))
+aumento = float(input('qual seu aumento em porcentos? '))
+novosalario = (salario * aumento/100) + salario
+print (f'seu novo salario vai ser {novosalario}')
---
+++ aulas/aula03.py
@@ -0,0 +1,8 @@
+a = float(input ('fala um número ai '))
+b = float(input ('fala outro numero ai '))
+if a > b:
+    print (f'o numero {a} e maior que o {b}')
+elif a < b:
+    print (f'o numero {b} e maior que o {a}')
+else:
+    print (f'os numeros sao iguais, ambos são {a}')
